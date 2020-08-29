fad2eeb1c57b7410421083f12b42e4be68249da7
Bruno Shiroma <brunoshiroma@gmail.com>
2020-08-28 22:15:54 -0300

    updated bigint lib, big improviments on performance \!, fixed docker build

--- src/benchtool/simple_fibonacci_loop_bench.rs
+++ src/benchtool/simple_fibonacci_loop_bench.rs
@@ -3,19 +3,16 @@ extern crate num_traits;
 
 use num_bigint::BigUint;
 use num_traits::{Zero, One};
-use std::mem::replace;
 
 pub fn bench(count: i32) -> BigUint{
 
-    let  mut current : BigUint  = Zero::zero();
+    let mut current : BigUint  = Zero::zero();
     let mut next: BigUint  = One::one();
 
     for _i in 0..count {
-
         let result = current + &next;
-        // see https://docs.rs/num-bigint/0.2.6/num_bigint/index.html
-        current = replace(&mut next, result);
-
+        current = next;
+        next = result
     }
 
     return current;
