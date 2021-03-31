840f43fb96dcbe2d8a705630294ddbd7a6817e51
Bruno Shiroma <brunoshiroma@gmail.com>
2021-03-30 21:27:14 -0300

    fixed warnings, removed travis archs ( arm, i386 )

--- src/benchtool/simple_fibonacci_recursive_bench.rs
+++ src/benchtool/simple_fibonacci_recursive_bench.rs
@@ -9,14 +9,10 @@ pub fn bench(count: i32) -> BigUint{
 
     let num: u64 = count as u64;
 
-    let mut result : BigUint = Zero::zero();
-
-
     let res = thread::Builder::new()
         .stack_size(num as usize * 1024)
         .spawn(move || {
-        result = calculate_fibonacci(Zero::zero(), One::one(), count, &mut 1);
-        result
+        return calculate_fibonacci(Zero::zero(), One::one(), count, &mut 1);
     });
 
     return res.unwrap().join().unwrap_or_default();
