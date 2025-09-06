903658e7e355ba271b446d0a55a30ec7d14aa851
Bruno Shiroma <brunoshiroma@gmail.com>
2025-09-06 15:27:41 +0000

    feat: update libs, dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,13 @@
-FROM alpine as buildbase
+FROM rust:alpine as buildbase
 
 WORKDIR /bench
-
-RUN apk update
-RUN apk add cargo
-RUN apk add rust
 
 COPY Cargo.* ./
 RUN cargo fetch
 
 COPY . .
 RUN cargo build --release --all-features
-
 FROM alpine as runtime
-
-RUN apk update
-RUN apk add libgcc
 
 WORKDIR /bench
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 mod benchtool;
 
 use std::os::raw::c_char;
+use std::ffi::CString;
 
-#[no_mangle]
-pub extern fn benchtool_rust_call(iteration: i32, repeat: i32, bench_type: i32) -> *const c_char{
-    benchtool::executor::benchtool_rust_execute(iteration, repeat, bench_type).as_ptr() as *const c_char
+#[unsafe(no_mangle)]
+pub extern "C" fn benchtool_rust_call(iteration: i32, repeat: i32, bench_type: i32) -> *const c_char{
+    let result = CString::new( benchtool::executor::benchtool_rust_execute(iteration, repeat, bench_type));
+    
+    match result {
+        Ok(value) => return value.into_raw(),
+        Err(_error) => return CString::from(c"ERROR").into_raw(),
+    }
 }
